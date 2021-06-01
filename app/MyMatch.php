28b77491609a1e67e9ba1e7a9319e951f878487d
titty91 <tznbaraldi@gmail.com>
2021-06-01 19:13:24 +0200

    tabella collegata e trascritta nel database

---
+++ app/MyMatch.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MyMatch extends Model
+{
+    protected $table = 'my_matches';
+    protected $fillable = [
+
+        'team1',
+        'team2',
+        'point1',
+        'point2',
+        'result',
+    ];
+    
+}
---
+++ database/factories/MyMatchFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\MyMatch;
+use Faker\Generator as Faker;
+
+$factory->define(MyMatch::class, function (Faker $faker) {
+    return [
+        'team1'  => $faker -> word,
+        'team2' => $faker ->  word,  
+        'point1' => rand(0, 30),
+        'point2' => rand(0, 30),
+        'result' =>  rand(0, 1) == 0
+    ];
+});
